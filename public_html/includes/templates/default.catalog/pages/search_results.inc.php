a7fa2f5c8d93c102d18929eec83b8c2c87b75056
zhangning_holley <909604945@qq.com>
2018-11-04 22:43:55 +0800

    完成了搜索页面、分类商品列表页面的 按条件排序和底部的分页样式修改

--- public_html/includes/templates/default.catalog/pages/search_results.inc.php
+++ public_html/includes/templates/default.catalog/pages/search_results.inc.php
@@ -3,7 +3,38 @@
      * 查询后的页面page
      */
 ?>
+<style type="text/css">
+    .sort_active {
+        font-size: 14px !important;
+        text-decoration: none !important;
+        background: #b2906a;
+        color: #fff !important;
+        border: 1px solid #9c7d60;
+        font-weight: bold;
+        padding: 10px 12px;
+        text-transform:Uppercase;
+        height:40px;
+    }
+    .sort_default {
+        font-size: 14px !important;
+        text-decoration: none !important;
+        background: #e0e0e0;
+        border: 1px solid #e0e0e0;
+        font-weight: bold;
+        padding: 10px 12px;
+        margin-left: 1px;
+        cursor: pointer;
+        text-transform:Uppercase;
+        height:40px;
+    }
+    .sort_main_div{
+        padding-right: 50px;
+        color:#000000;
+    }
+    a:link{color: #000000;}
+    a:visited{color: #000000;}
 
+</style>
 <aside id="sidebar">
   <?php include vmod::check(FS_DIR_HTTP_ROOT . WS_DIR_TEMPLATE . 'views/column_left.inc.php'); ?>
 </aside>
@@ -15,14 +46,14 @@
   <div id="box-search-results" class="box">
 
     <?php if ($products) { ?>
-    <div class="btn-group pull-right hidden-xs">
+    <div class="btn-group pull-right hidden-xs sort_main_div">
 <?php
   $separator = false;
   foreach ($sort_alternatives as $key => $value) {
     if ($_GET['sort'] == $key) {
-      echo '<span class="btn btn-default active">'. $value .'</span>';
+      echo '<span class="sort_active">'. $value .'</span>';
     } else {
-      echo '<a class="btn btn-default" href="'. document::href_ilink(null, array('sort' => $key), true) .'">'. $value .'</a>';
+      echo '<a class="sort_default" href="'. document::href_ilink(null, array('sort' => $key), true) .'">'. $value .'</a>';
     }
   }
 ?>
--- public_html/includes/templates/default.catalog/views/pagination.inc.php
+++ public_html/includes/templates/default.catalog/views/pagination.inc.php
@@ -1,9 +1,83 @@
-<ul class="pagination">
-  <?php foreach ($items as $item) { ?>
-    <?php if ($item['disabled']) { ?>
-    <li class="disabled"><span><?php echo $item['title']; ?></span></li>
-    <?php } else { ?>
-    <li<?php if ($item['active']) echo ' class="active"'; ?>><a href="<?php echo htmlspecialchars($item['link']); ?>"><?php echo $item['title']; ?></a></li>
+<?php
+    /**
+     * //TODO: 列表底部分页的视图
+     */
+?>
+<style type="text/css">
+    .pagination_active {
+        font-size: 14px !important;
+        text-decoration: none !important;
+        background: #b2906a;
+        color: #fff !important;
+        border: 1px solid #9c7d60;
+        font-weight: bold;
+        padding: 10px 10px;
+        text-transform:Uppercase;
+        height:40px;
+    }
+    .pagination_active>a {
+        color:#FFFFFF;
+    }
+    .pagination_default {
+        font-size: 14px !important;
+        text-decoration: none !important;
+        background: #e0e0e0;
+        border: 1px solid #e0e0e0;
+        font-weight: bold;
+        padding: 10px 10px;
+        margin-left: 1px;
+        cursor: pointer;
+        text-transform:Uppercase;
+        height:40px;
+    }
+    .disabled {
+        height:40px;
+        opacity: .5;
+        cursor: not-allowed;
+        font-size: 14px !important;
+        text-decoration: none !important;
+        background: #e0e0e0;
+        border: 1px solid #e0e0e0;
+        font-weight: bold;
+        padding: 10px 10px;
+        margin-left: 1px;
+        text-transform:Uppercase;
+        height:40px;
+   }
+    .pagination_main {
+        display: inline-flex;
+        padding-left: 0;
+        margin: 15px 0;
+        border-radius: 4px;
+        list-style: none;
+    }
+    /*a:link{color: #000000;}*/
+    </style>
+
+<ul class="pagination_main">
+    <?php foreach ($items as $item) { ?>
+        <?php if ($item['disabled']) { ?>
+            <li class="disabled"><span><?php echo $item['title']; ?></span></li>
+        <?php } else { ?>
+            <li<?php if ($item['active']) {
+                echo ' class="pagination_active"';
+            } else {
+                echo ' class="pagination_default"';
+            }
+            ?>>
+                <a href="<?php echo htmlspecialchars($item['link']); ?>"><?php echo $item['title']; ?></a></li>
+        <?php } ?>
     <?php } ?>
-  <?php } ?>
 </ul>
+
+<!--<ul class="pagination">-->
+<!--  --><?php //foreach ($items as $item) { ?>
+<!--    --><?php //if ($item['disabled']) { ?>
+<!--    <li class="disabled"><span>--><?php //echo $item['title']; ?><!--</span></li>-->
+<!--    --><?php //} else { ?>
+<!--    <li--><?php //if ($item['active']) echo ' class="active"'; ?>
+<!--    >-->
+<!--        <a href="--><?php //echo htmlspecialchars($item['link']); ?><!--">--><?php //echo $item['title']; ?><!--</a></li>-->
+<!--    --><?php //} ?>
+<!--  --><?php //} ?>
+<!--</ul>-->
