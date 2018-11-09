4c6fb0a6771bbb870d2746ae5b6184a279639c80
zhangning_holley <909604945@qq.com>
2018-11-09 10:13:53 +0800

    注释了box_filter.inc.php里的部分代码。

--- public_html/includes/templates/default.catalog/views/box_filter.inc.php
+++ public_html/includes/templates/default.catalog/views/box_filter.inc.php
@@ -1,24 +1,22 @@
 <div id="box-filter">
   <?php echo functions::form_draw_form_begin('filter_form', 'get'); ?>
-
-  <?php if ($manufacturers) { ?>
-  <div class="box manufacturers">
-    <h2 class="title"><?php echo language::translate('title_manufacturers', 'Manufacturers'); ?></h2>
-    <div class="form-control">
-      <ul class="list-unstyled">
-        <?php foreach ($manufacturers as $manufacturer) echo '<li><label>'. functions::form_draw_checkbox('manufacturers[]', $manufacturer['id'], true) .' '. $manufacturer['name'] .'</label></li>' . PHP_EOL; ?>
-      </ul>
-    </div>
-  </div>
-  <?php } ?>
-
+<!--  --><?php //if ($manufacturers) { ?>
+<!--  <div class="box manufacturers">-->
+<!--    <h2 class="title">--><?php //echo language::translate('title_manufacturers', 'Manufacturers'); ?><!--</h2>-->
+<!--    <div class="form-control">-->
+<!--      <ul class="list-unstyled">-->
+<!--        --><?php //foreach ($manufacturers as $manufacturer) echo '<li><label>'. functions::form_draw_checkbox('manufacturers[]', $manufacturer['id'], true) .' '. $manufacturer['name'] .'</label></li>' . PHP_EOL; ?>
+<!--      </ul>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--  --><?php //} ?>
   <?php if ($product_groups) { ?>
   <?php foreach ($product_groups as $group) { ?>
   <div class="box product-group" data-id="<?php echo $group['id']; ?>">
     <h2 class="title"><?php echo $group['name']; ?></h2>
     <div class="form-control">
       <ul class="list-unstyled">
-        <?php foreach ($group['values'] as $value) echo '<li><label>' . functions::form_draw_checkbox('product_groups[]', $group['id'].'-'.$value['id']) .' '. $value['name'].'</label></li>' . PHP_EOL; ?>
+        <?php foreach ($group['values'] as $value) echo '<li><label>' . functions::form_draw_checkbox('product_groups[]', $group['id'].'-'.$value['id'],"class='xxx'") .' '. $value['name'].'</label></li>' . PHP_EOL; ?>
       </ul>
     </div>
   </div>
